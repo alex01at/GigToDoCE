fd85841e9b511e6eb9e6c296b95b44cb9a1d6a39
alex01at <alex01at@gmail.com>
2023-01-06 16:19:56 +0100

    admin footer copyright

--- admin/assets/js/minimal.js
+++ admin/assets/js/minimal.js
@@ -1,4 +1,21 @@
-$('body').append('<section class="clearfix post_footer">@ Copyright GigToDoScript 2020. Pixinal Studio. </section>');
+/*
+ *   Copyright (c) 2023 Alexander Prossliner - https://www.gigtodo.net
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+$('body').append('<section class="clearfix post_footer">@ Copyright GigToDo</section>');
 
 // $(document).ready(function(){
 
